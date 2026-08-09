c4f5f37119d29b93663b29cf5f4da4b10a37b9c3
erknhct <118633765+erknhct@users.noreply.github.com>
2026-08-09 23:41:10 +0300

    GÜNCELLEME 9

--- BASLAT.bat
+++ BASLAT.bat
@@ -1,24 +1,15 @@
 @echo off
 title Izin Sistemi Sunucu - Kapatmayin!
+echo Izin Otomasyonu Baslatiliyor... Lutfen bekleyiniz...
 
 :: Proje klasorune git
 cd /d "%~dp0"
 
 :: node_modules yoksa kur
 if not exist "node_modules" (
+    echo [!] Kurulum yapiliyor, bu islem biraz surebilir...
     npm install
 )
 
-:: IP ve hostname al
-for /f "tokens=*" %%i in ('hostname') do set HOSTNAME=%%i
-for /f "tokens=2 delims=:" %%a in ('ipconfig ^| findstr "IPv4"') do (
-    set IP=%%a
-    goto :found
-)
-:found
-set IP=%IP: =%
-
-
-
-:: Sunucuyu arka planda baslat - bu pencere kucuk kalsin
+:: Sunucuyu baslat
 npm run start
